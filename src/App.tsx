c78c9c315667a949f11eb0e3ae6f304fe0aeb016
Salah Moussaoui <60707540+moussaouisalah@users.noreply.github.com>
2022-03-30 15:58:42 +0000

    added some code splitting
    routes are now lazy loaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Home from "./container/Home";
-import RegisterRedirect from "./container/RegisterRedirect";
-import UrlStats from "./container/UrlStats";
-import Dashboard from "./container/Dashboard";
+import { lazy, Suspense } from "react";
 import { ToastContainer } from "react-toastify";
+
 import ErrorBoundary from "./components/ErrorBoundary";
+import Spinner from "./components/Spinner";
+
+const Home = lazy(() => import("./container/Home"));
+const RegisterRedirect = lazy(() => import("./container/RegisterRedirect"));
+const Dashboard = lazy(() => import("./container/Dashboard"));
+const UrlStats = lazy(() => import("./container/UrlStats"));
 
 function App() {
   return (
     <BrowserRouter>
       <ErrorBoundary>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="stats/:id" element={<UrlStats />} />
-          <Route path=":id" element={<RegisterRedirect />} />
-          <Route path="dashboard" element={<Dashboard />} />
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="page__container flex justify-center items-center">
+              <Spinner />
+            </div>
+          }
+        >
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="stats/:id" element={<UrlStats />} />
+            <Route path=":id" element={<RegisterRedirect />} />
+            <Route path="dashboard" element={<Dashboard />} />
+          </Routes>
+        </Suspense>
         <ToastContainer />
       </ErrorBoundary>
     </BrowserRouter>
